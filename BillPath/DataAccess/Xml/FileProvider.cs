5c0272b561f8a6d7e840ed5f276ca67a68b46eb4
Andrei Fangli <andrei_fangli@outlook.com>
2015-08-26 23:27:59 +0300

    Task/#18 Added FileProvider infrastructure

---
+++ BillPath/DataAccess/Xml/FileProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace BillPath.DataAccess.Xml
+{
+    public abstract class FileProvider
+    {
+        public abstract Task<bool> FileExistsAsync();
+        public abstract Task<Stream> GetReadStreamAsync();
+        public abstract Task<Stream> GetWriteStreamAsync();
+
+        public string FileName
+        {
+            get;
+            set;
+        }
+    }
+}
