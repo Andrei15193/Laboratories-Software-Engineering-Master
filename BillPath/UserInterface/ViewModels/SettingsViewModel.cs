60bb699f61956ac0fe30d4062e55e756b68f1bfb
Andrei Fangli <andrei_fangli@outlook.com>
2015-08-26 23:30:11 +0300

    Task/#18 Added all settings to SettingsViewModel

--- BillPath/UserInterface/ViewModels/SettingsViewModel.cs
+++ BillPath/UserInterface/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
 using BillPath.DataAccess;
@@ -9,6 +10,122 @@ namespace BillPath.UserInterface.ViewModels
     public class SettingsViewModel
         : ViewModel
     {
+        private static readonly IEnumerable<Currency> _allCurrencies =
+            new[]
+            {
+                new Currency(new RegionInfo("ar-AE")),
+                new Currency(new RegionInfo("prs-AF")),
+                new Currency(new RegionInfo("uz-Arab-AF")),
+                new Currency(new RegionInfo("sq-AL")),
+                new Currency(new RegionInfo("hy-AM")),
+                new Currency(new RegionInfo("es-AR")),
+                new Currency(new RegionInfo("en-AU")),
+                new Currency(new RegionInfo("az-Cyrl-AZ")),
+                new Currency(new RegionInfo("bs-Cyrl-BA")),
+                new Currency(new RegionInfo("bn-BD")),
+                new Currency(new RegionInfo("bg-BG")),
+                new Currency(new RegionInfo("ar-BH")),
+                new Currency(new RegionInfo("ms-BN")),
+                new Currency(new RegionInfo("es-BO")),
+                new Currency(new RegionInfo("pt-BR")),
+                new Currency(new RegionInfo("dz-BT")),
+                new Currency(new RegionInfo("tn-BW")),
+                new Currency(new RegionInfo("be-BY")),
+                new Currency(new RegionInfo("en-BZ")),
+                new Currency(new RegionInfo("en-CA")),
+                new Currency(new RegionInfo("fr-CD")),
+                new Currency(new RegionInfo("de-CH")),
+                new Currency(new RegionInfo("arn-CL")),
+                new Currency(new RegionInfo("bo-CN")),
+                new Currency(new RegionInfo("es-CO")),
+                new Currency(new RegionInfo("es-CR")),
+                new Currency(new RegionInfo("es-CU")),
+                new Currency(new RegionInfo("cs-CZ")),
+                new Currency(new RegionInfo("da-DK")),
+                new Currency(new RegionInfo("es-DO")),
+                new Currency(new RegionInfo("ar-DZ")),
+                new Currency(new RegionInfo("ar-EG")),
+                new Currency(new RegionInfo("ti-ER")),
+                new Currency(new RegionInfo("am-ET")),
+                new Currency(new RegionInfo("br-FR")),
+                new Currency(new RegionInfo("cy-GB")),
+                new Currency(new RegionInfo("ka-GE")),
+                new Currency(new RegionInfo("es-GT")),
+                new Currency(new RegionInfo("en-HK")),
+                new Currency(new RegionInfo("es-HN")),
+                new Currency(new RegionInfo("hr-HR")),
+                new Currency(new RegionInfo("fr-HT")),
+                new Currency(new RegionInfo("hu-HU")),
+                new Currency(new RegionInfo("en-ID")),
+                new Currency(new RegionInfo("he-IL")),
+                new Currency(new RegionInfo("as-IN")),
+                new Currency(new RegionInfo("ar-IQ")),
+                new Currency(new RegionInfo("fa-IR")),
+                new Currency(new RegionInfo("is-IS")),
+                new Currency(new RegionInfo("en-JM")),
+                new Currency(new RegionInfo("ar-JO")),
+                new Currency(new RegionInfo("ja-JP")),
+                new Currency(new RegionInfo("sw-KE")),
+                new Currency(new RegionInfo("ky-KG")),
+                new Currency(new RegionInfo("km-KH")),
+                new Currency(new RegionInfo("ko-KR")),
+                new Currency(new RegionInfo("ar-KW")),
+                new Currency(new RegionInfo("kk-KZ")),
+                new Currency(new RegionInfo("lo-LA")),
+                new Currency(new RegionInfo("ar-LB")),
+                new Currency(new RegionInfo("si-LK")),
+                new Currency(new RegionInfo("ar-LY")),
+                new Currency(new RegionInfo("ar-MA")),
+                new Currency(new RegionInfo("ro-MD")),
+                new Currency(new RegionInfo("mk-MK")),
+                new Currency(new RegionInfo("my-MM")),
+                new Currency(new RegionInfo("mn-MN")),
+                new Currency(new RegionInfo("zh-MO")),
+                new Currency(new RegionInfo("dv-MV")),
+                new Currency(new RegionInfo("es-MX")),
+                new Currency(new RegionInfo("en-MY")),
+                new Currency(new RegionInfo("bin-NG")),
+                new Currency(new RegionInfo("es-NI")),
+                new Currency(new RegionInfo("nb-NO")),
+                new Currency(new RegionInfo("ne-NP")),
+                new Currency(new RegionInfo("en-NZ")),
+                new Currency(new RegionInfo("ar-OM")),
+                new Currency(new RegionInfo("es-PA")),
+                new Currency(new RegionInfo("es-PE")),
+                new Currency(new RegionInfo("en-PH")),
+                new Currency(new RegionInfo("pa-Arab-PK")),
+                new Currency(new RegionInfo("pl-PL")),
+                new Currency(new RegionInfo("es-PY")),
+                new Currency(new RegionInfo("ar-QA")),
+                new Currency(new RegionInfo("ro-RO")),
+                new Currency(new RegionInfo("sr-Cyrl-RS")),
+                new Currency(new RegionInfo("ba-RU")),
+                new Currency(new RegionInfo("rw-RW")),
+                new Currency(new RegionInfo("ar-SA")),
+                new Currency(new RegionInfo("se-SE")),
+                new Currency(new RegionInfo("en-SG")),
+                new Currency(new RegionInfo("so-SO")),
+                new Currency(new RegionInfo("ar-SY")),
+                new Currency(new RegionInfo("th-TH")),
+                new Currency(new RegionInfo("tg-Cyrl-TJ")),
+                new Currency(new RegionInfo("tk-TM")),
+                new Currency(new RegionInfo("ar-TN")),
+                new Currency(new RegionInfo("tr-TR")),
+                new Currency(new RegionInfo("en-TT")),
+                new Currency(new RegionInfo("zh-TW")),
+                new Currency(new RegionInfo("uk-UA")),
+                new Currency(new RegionInfo("chr-Cher-US")),
+                new Currency(new RegionInfo("es-UY")),
+                new Currency(new RegionInfo("uz-Cyrl-UZ")),
+                new Currency(new RegionInfo("es-VE")),
+                new Currency(new RegionInfo("vi-VN")),
+                new Currency(new RegionInfo("fr-CM")),
+                new Currency(new RegionInfo("en-029")),
+                new Currency(new RegionInfo("es-419")),
+                new Currency(new RegionInfo("ff-Latn-SN")),
+                new Currency(new RegionInfo("ar-YE")),
+                new Currency(new RegionInfo("af-ZA"))
+            };
         private readonly Settings _settings;
 
         public SettingsViewModel()
@@ -50,6 +167,13 @@ namespace BillPath.UserInterface.ViewModels
                 OnPropertyChanged();
             }
         }
+        public IEnumerable<Currency> AllCurrencies
+        {
+            get
+            {
+                return _allCurrencies;
+            }
+        }
 
         public AsyncCommand LoadCommand
         {
@@ -59,6 +183,5 @@ namespace BillPath.UserInterface.ViewModels
         {
             get;
         }
-        public object Thread { get; private set; }
     }
 }
