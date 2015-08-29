f86b1a74491c9b8e6587edd83ddf555a19ba400a
Andrei Fangli <andrei_fangli@outlook.com>
2015-08-29 14:24:14 +0300

    Task/#23 Added ComboBox control for currency display mode and changed ComboBox header to be used as input label.

---
+++ BillPath/Models/CurrencyDisplayFormatters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BillPath.Models
+{
+    public static class CurrencyDisplayFormatters
+    {
+        public static ICurrencyFormatter GetFor(CurrencyDisplayFormat currencyDisplayFormat)
+        {
+            switch (currencyDisplayFormat)
+            {
+                case CurrencyDisplayFormat.Full:
+                    return new CurrencySynmbolAndIsoCodeFormatter();
+
+                case CurrencyDisplayFormat.Symbol:
+                    return new CurrencySymbolOnlyFormatter();
+
+                case CurrencyDisplayFormat.IsoCode:
+                    return new CurrencyIsoCodeOnlyFormatter();
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
