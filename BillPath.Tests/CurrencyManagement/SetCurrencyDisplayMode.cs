59a64b3510b02afe2a24dec4cf76bca1cbc7c29c
Andrei Fangli <andrei_fangli@outlook.com>
2015-08-29 11:05:24 +0300

    Task/#17 Added currency display mode scenarios

---
+++ BillPath.Tests/CurrencyManagement/SetCurrencyDisplayMode.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using BillPath.Models;
+using TechTalk.SpecFlow;
+
+namespace BillPath.Tests.CurrencyManagement
+{
+    [Binding]
+    [Scope(Feature = "SetCurrencyDisplayMode")]
+    public sealed class SetCurrencyDisplayMode
+    {
+        private Currency _currency;
+
+        [Given(@"(\w+(?:-\w+)?) currency")]
+        public void GivenCurrency(string regionName)
+        {
+            _currency = new Currency(new RegionInfo(regionName));
+        }
+
+        [When("I display currency with symbol only")]
+        public void WhenSymbolOnlyDisplayIsSet()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        [When("I display currency with ISO code only")]
+        public void WhenIsoCodeOnlyDisplayIsSet()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        [When("I display currency with symbol and ISO code")]
+        public void WhenFullDisplayIsSet()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenFormattedCurrencyIs(string value)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
