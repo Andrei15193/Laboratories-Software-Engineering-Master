1949193f394b500d1d9c1e34d2115ffcfc42f138
Andrei Fangli <andrei_fangli@outlook.com>
2015-09-05 11:21:51 +0300

    Task/#34 Added scenarios for add income feature

---
+++ BillPath.Tests/IncomeManagement/AddIncome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace BillPath.Tests.IncomeManagement
+{
+    [Binding]
+    [Scope(Feature = "AddIncome")]
+    public sealed class AddIncome
+    {
+        [Given("no incomes")]
+        public void GivenNoIncomes()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I add a new income in (\w+(?:-\w+)?) currency with the amount of (\d+(?:\.\d+)?) on (\d{1,2}/\d{1,2}/\d{4} \d{1,2}:\d{1,2}:\d{1,2}) and '(.*)' description")]
+        public void WhenAddingNewIncome(string regionName, decimal amount, DateTime transactionDate, string description)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"there should be a total of (\d+) incomes")]
+        public void ThenIncomeCountIs(int incomeCount)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        [Then(@"the total income in (\w+(?:-\w+)?) currency should be (\d+(?:\.\d+)?)")]
+        public void ThenTotalIncomeAmountIs(string regionName, decimal totalIncomeAmount)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        [Then(@"the available funds in (\w+(?:-\w+)?) currency should be (\d+(?:\.\d+)?)")]
+        public void ThenTotalAvailableAccountIs(string regionName, decimal totalAvailableAmount)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
