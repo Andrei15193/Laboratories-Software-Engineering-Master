8c9396b09576947f53381104b7ecf5365bb48a0d
Andrei Fangli <andrei_fangli@outlook.com>
2015-09-15 22:42:36 +0300

    Task/#49 added Amount

---
+++ BillPath.Tests/Model/AmountTests.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Globalization;
+using BillPath.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BillPath.Tests.Model
+{
+    [TestClass]
+    public class AmountTests
+    {
+        private enum Relationship
+        {
+            Equal = 1,
+            LessThan,
+            GreaterThan
+        }
+
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+        [TestMethod]
+        [DataSource("AmountTests")]
+        [DeploymentItem(@"Model\AmountTests.xml", "Model")]
+        public void TestAddingTwoAmountsInSameCurrency()
+        {
+            var first = (decimal)TestContext.DataRow["first"];
+            var second = (decimal)TestContext.DataRow["second"];
+            var sum = (decimal)TestContext.DataRow["sum"];
+            var result = (new Amount(first, default(Currency)) + new Amount(second, default(Currency))).Value;
+
+            Assert.AreEqual(sum, result);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestAddingTwoAmountsInDifferentCurrencies()
+        {
+            var result = new Amount(12, new Currency(new RegionInfo("RO")))
+                + new Amount(10, new Currency(new RegionInfo("GB")));
+        }
+
+        [TestMethod]
+        [DataSource("AmountTests")]
+        [DeploymentItem(@"Model\AmountTests.xml", "Model")]
+        public void TestSubtractingTwoAmountsInSameCurrency()
+        {
+            var first = (decimal)TestContext.DataRow["first"];
+            var second = (decimal)TestContext.DataRow["second"];
+            var difference = (decimal)TestContext.DataRow["difference"];
+            var result = (new Amount(first, default(Currency)) - new Amount(second, default(Currency))).Value;
+
+            Assert.AreEqual(difference, result);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestSubtractingTwoAmountsInDifferentCurrencies()
+        {
+            var result = new Amount(12, new Currency(new RegionInfo("RO")))
+               - new Amount(10, new Currency(new RegionInfo("GB")));
+        }
+
+        [TestMethod]
+        [DataSource("AmountTests")]
+        [DeploymentItem(@"Model\AmountTests.xml", "Model")]
+        public void TestMultiplyingTwoAmountsInSameCurrency()
+        {
+            var first = (decimal)TestContext.DataRow["first"];
+            var second = (decimal)TestContext.DataRow["second"];
+            var product = (decimal)TestContext.DataRow["product"];
+            var result = (new Amount(first, default(Currency)) * new Amount(second, default(Currency))).Value;
+
+            Assert.AreEqual(product, result);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestMultiplyingTwoAmountsInDifferentCurrencies()
+        {
+            var result = new Amount(12, new Currency(new RegionInfo("RO")))
+               * new Amount(10, new Currency(new RegionInfo("GB")));
+        }
+
+        [TestMethod]
+        [DataSource("AmountTests")]
+        [DeploymentItem(@"Model\AmountTests.xml", "Model")]
+        public void TestDividingTwoAmountsInSameCurrency()
+        {
+            var first = (decimal)TestContext.DataRow["first"];
+            var second = (decimal)TestContext.DataRow["second"];
+            var quotient = (decimal)TestContext.DataRow["quotient"];
+            var result = (new Amount(first, default(Currency)) / new Amount(second, default(Currency))).Value;
+
+            Assert.AreEqual(quotient, result);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDividingTwoAmountsInDifferentCurrencies()
+        {
+            var result = new Amount(12, new Currency(new RegionInfo("RO")))
+               / new Amount(10, new Currency(new RegionInfo("GB")));
+        }
+
+        [TestMethod]
+        [DataSource("AmountTests")]
+        [DeploymentItem(@"Model\AmountTests.xml", "Model")]
+        public void TestRelationshipInSameCurrency()
+        {
+            var first = new Amount((decimal)TestContext.DataRow["first"], default(Currency));
+            var second = new Amount((decimal)TestContext.DataRow["second"], default(Currency));
+
+            switch (_GetRelationship())
+            {
+                case Relationship.LessThan:
+                    Assert.IsTrue(first < second, $"{first.Value} < {second.Value}");
+                    Assert.IsTrue(first <= second, $"{first.Value} <= {second.Value}");
+                    Assert.IsFalse(first == second, $"{first.Value} == {second.Value}");
+                    Assert.IsTrue(first != second, $"{first.Value} != {second.Value}");
+                    Assert.IsFalse(first >= second, $"{first.Value} >= {second.Value}");
+                    Assert.IsFalse(first > second, $"{first.Value} > {second.Value}");
+                    break;
+
+                case Relationship.Equal:
+                    Assert.IsFalse(first < second, $"{first.Value} < {second.Value}");
+                    Assert.IsTrue(first <= second, $"{first.Value} <= {second.Value}");
+                    Assert.IsTrue(first == second, $"{first.Value} == {second.Value}");
+                    Assert.IsFalse(first != second, $"{first.Value} != {second.Value}");
+                    Assert.IsTrue(first >= second, $"{first.Value} >= {second.Value}");
+                    Assert.IsFalse(first > second, $"{first.Value} > {second.Value}");
+                    break;
+
+                case Relationship.GreaterThan:
+                    Assert.IsFalse(first < second, $"{first.Value} < {second.Value}");
+                    Assert.IsFalse(first <= second, $"{first.Value} <= {second.Value}");
+                    Assert.IsFalse(first == second, $"{first.Value} == {second.Value}");
+                    Assert.IsTrue(first != second, $"{first.Value} != {second.Value}");
+                    Assert.IsTrue(first >= second, $"{first.Value} >= {second.Value}");
+                    Assert.IsTrue(first > second, $"{first.Value} > {second.Value}");
+                    break;
+
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+        [TestMethod]
+        public void TestEqualInDifferentCurrency()
+        {
+            Assert.IsFalse(
+                new Amount(12, new Currency(new RegionInfo("RO")))
+                == new Amount(10, new Currency(new RegionInfo("GB"))));
+        }
+        [TestMethod]
+        public void TestNotEqualInDifferentCurrency()
+        {
+            Assert.IsTrue(
+                new Amount(12, new Currency(new RegionInfo("RO")))
+                != new Amount(10, new Currency(new RegionInfo("GB"))));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestLessThanInDifferentCurrency()
+        {
+            var result = new Amount(12, new Currency(new RegionInfo("RO")))
+               < new Amount(10, new Currency(new RegionInfo("GB")));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestLessThanOrEqualInDifferentCurrency()
+        {
+            var result = new Amount(12, new Currency(new RegionInfo("RO")))
+               <= new Amount(10, new Currency(new RegionInfo("GB")));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestGreaterThanEqualInDifferentCurrency()
+        {
+            var result = new Amount(12, new Currency(new RegionInfo("RO")))
+               >= new Amount(10, new Currency(new RegionInfo("GB")));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestGreaterThanInDifferentCurrency()
+        {
+            var result = new Amount(12, new Currency(new RegionInfo("RO")))
+               > new Amount(10, new Currency(new RegionInfo("GB")));
+        }
+
+        private Relationship _GetRelationship()
+        {
+            return (Relationship)Enum.Parse(typeof(Relationship), (string)TestContext.DataRow["relationship"], true);
+        }
+    }
+}
---
+++ BillPath/Models/Amount.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace BillPath.Models
+{
+    public struct Amount
+        : IEquatable<Amount>, IComparable<Amount>
+    {
+        public Amount(decimal value, Currency currency)
+        {
+            Value = value;
+            Currency = currency;
+        }
+
+        public decimal Value
+        {
+            get;
+        }
+        public Currency Currency
+        {
+            get;
+        }
+
+        public bool Equals(Amount other)
+            => (Value == other.Value && Currency == other.Currency);
+        public override bool Equals(object obj)
+        {
+            var other = obj as Amount?;
+
+            return (other != null && Equals(other.Value));
+        }
+        public override int GetHashCode()
+            => Value.GetHashCode() ^ Currency.GetHashCode();
+        public static bool operator ==(Amount left, Amount right)
+            => left.Equals(right);
+        public static bool operator !=(Amount left, Amount right)
+            => !left.Equals(right);
+
+        public int CompareTo(Amount other)
+        {
+            _ValidateCurrency(this, other, nameof(other));
+
+            return Value.CompareTo(other.Value);
+        }
+        public static bool operator <(Amount left, Amount right)
+        {
+            _ValidateCurrency(left, right);
+
+            return left.CompareTo(right) < 0;
+        }
+        public static bool operator >(Amount left, Amount right)
+        {
+            _ValidateCurrency(left, right);
+
+            return left.CompareTo(right) > 0;
+        }
+        public static bool operator <=(Amount left, Amount right)
+        {
+            _ValidateCurrency(left, right);
+
+            return (left == right || left < right);
+        }
+        public static bool operator >=(Amount left, Amount right)
+        {
+            _ValidateCurrency(left, right);
+
+            return (left == right || left > right);
+        }
+
+        public static Amount operator +(Amount left, Amount right)
+        {
+            _ValidateCurrency(left, right);
+
+            return new Amount(left.Value + right.Value, left.Currency);
+        }
+        public static Amount operator -(Amount left, Amount right)
+        {
+            _ValidateCurrency(left, right);
+
+            return new Amount(left.Value - right.Value, left.Currency);
+        }
+        public static Amount operator *(Amount left, Amount right)
+        {
+            _ValidateCurrency(left, right);
+
+            return new Amount(left.Value * right.Value, left.Currency);
+        }
+        public static Amount operator /(Amount left, Amount right)
+        {
+            _ValidateCurrency(left, right);
+
+            return new Amount(left.Value / right.Value, left.Currency);
+        }
+
+        private static void _ValidateCurrency(Amount first, Amount second, string paramName = null)
+        {
+            if (first.Currency != second.Currency)
+                throw new ArgumentException("Can only add amounts expressed in same currency", paramName);
+        }
+    }
+}
