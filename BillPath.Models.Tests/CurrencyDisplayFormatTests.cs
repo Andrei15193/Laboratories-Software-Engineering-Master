b2a30d6012e91a5e31b6841042c6bcedf23da048
Andrei Fangli <andrei_fangli@outlook.com>
2015-10-04 12:23:11 +0300

    Task/#49 Added CurrencyDisplayFormat tests.

---
+++ BillPath.Models.Tests/CurrencyDisplayFormatTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace BillPath.Models.Tests
+{
+    [TestClass]
+    public class CurrencyDisplayFormatTests
+    {
+        [TestMethod]
+        public void TestHasDefaultMember()
+        {
+            Assert.IsTrue(Enum.IsDefined(typeof(CurrencyDisplayFormat), default(CurrencyDisplayFormat)));
+        }
+    }
+}
