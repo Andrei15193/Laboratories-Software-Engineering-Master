1cef966c10658e033d6f3739fd4d0006bb0882f2
Andrei Fangli <andrei_fangli@outlook.com>
2015-10-09 23:49:15 +0300

    Task/#49 Added CurrencyProvider

---
+++ BillPath.Providers.Tests/CurrencyProviderTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace BillPath.Providers.Tests
+{
+    [TestClass]
+    public class CurrencyProviderTests
+    {
+        [TestMethod]
+        public void TestProviderDoesNotReturnNull()
+        {
+            var currencyProvider = new CurrencyProvider();
+
+            Assert.IsNotNull(currencyProvider.Currencies);
+        }
+
+        [TestMethod]
+        public void TestProviderReturnsDistinctCurrencies()
+        {
+            var currencyProvider = new CurrencyProvider();
+
+            Assert.AreEqual(
+                currencyProvider.Currencies.Count(),
+                currencyProvider.Currencies.Distinct().Count(),
+                string.Join(
+                    Environment.NewLine,
+                    new[] { "The following currencies are duplicates:" }.Concat(
+                        from currency in currencyProvider.Currencies
+                        group currency by currency into currencies
+                        where currencies.Skip(1).Any()
+                        select string.Join(", ", currencies))));
+        }
+
+        [DataTestMethod]
+        [DataRow(153)]
+        public void TestProviderReturnsAllCurrencies(int currencyCount)
+        {
+            var currencyProvider = new CurrencyProvider();
+
+            Assert.AreEqual(currencyCount, currencyProvider.Currencies.Count());
+        }
+    }
+}
---
+++ Get-Currencies.ps1
@@ -0,0 +1,22 @@
+﻿[System.Collections.Generic.List[System.String]] $cultureNames = New-Object 'System.Collections.Generic.List[System.String]'
+[System.Collections.Generic.HashSet[System.String]] $addedCurrencyIsoCodes = New-Object 'System.Collections.Generic.HashSet[System.String]' ([System.StringComparer]::OrdinalIgnoreCase)
+
+foreach ($cultureInfo in [System.Globalization.CultureInfo]::GetCultures([System.Globalization.CultureTypes]::AllCultures))
+{
+    try
+    {
+        [void](New-Object 'System.Globalization.RegionInfo' $cultureInfo.Name);
+        if ($addedCurrencyIsoCodes.Add((New-Object 'System.Globalization.RegionInfo' $cultureInfo.Name).ISOCurrencySymbol))
+        {
+            [void]$cultureNames.Add($cultureInfo.Name)
+        }
+    }
+    catch
+    {
+    }
+}
+
+foreach ($cultureName in $cultureNames)
+{
+    Write-Output "yield return new Currency(new RegionInfo(""$cultureName""));"
+}
