1b72a25fde70d86086869d56398892f6f9db5515
Andrei Fangli <andrei_fangli@outlook.com>
2015-10-10 10:14:59 +0300

    Task/#49 Refactor: CurrencyProviderTests.

--- BillPath.Providers.Tests/CurrencyProviderTests.cs
+++ BillPath.Providers.Tests/CurrencyProviderTests.cs
@@ -7,38 +7,28 @@ namespace BillPath.Providers.Tests
     [TestClass]
     public class CurrencyProviderTests
     {
+        private static CurrencyProvider Provider { get; } = new CurrencyProvider();
+
         [TestMethod]
         public void TestProviderDoesNotReturnNull()
-        {
-            var currencyProvider = new CurrencyProvider();
-
-            Assert.IsNotNull(currencyProvider.Currencies);
-        }
+            => Assert.IsNotNull(Provider.Currencies);
 
         [TestMethod]
         public void TestProviderReturnsDistinctCurrencies()
-        {
-            var currencyProvider = new CurrencyProvider();
-
-            Assert.AreEqual(
-                currencyProvider.Currencies.Count(),
-                currencyProvider.Currencies.Distinct().Count(),
+            => Assert.AreEqual(
+                Provider.Currencies.Count(),
+                Provider.Currencies.Distinct().Count(),
                 string.Join(
                     Environment.NewLine,
                     new[] { "The following currencies are duplicates:" }.Concat(
-                        from currency in currencyProvider.Currencies
+                        from currency in Provider.Currencies
                         group currency by currency into currencies
                         where currencies.Skip(1).Any()
                         select string.Join(", ", currencies))));
-        }
 
         [DataTestMethod]
         [DataRow(153)]
         public void TestProviderReturnsAllCurrencies(int currencyCount)
-        {
-            var currencyProvider = new CurrencyProvider();
-
-            Assert.AreEqual(currencyCount, currencyProvider.Currencies.Count());
-        }
+            => Assert.AreEqual(currencyCount, Provider.Currencies.Count());
     }
 }
