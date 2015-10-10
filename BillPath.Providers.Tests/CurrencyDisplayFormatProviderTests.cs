50045950fc78d49d280696b317d64cc93a8f9b4d
Andrei Fangli <andrei_fangli@outlook.com>
2015-10-10 10:13:15 +0300

    Task/#49 Added CurrencyDisplayFormatProvider.

---
+++ BillPath.Providers.Tests/CurrencyDisplayFormatProviderTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using BillPath.Models;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace BillPath.Providers.Tests
+{
+    [TestClass]
+    public class CurrencyDisplayFormatProviderTests
+    {
+        private static CurrencyDisplayFormatProvider Provider { get; } = new CurrencyDisplayFormatProvider();
+
+        [TestMethod]
+        public void TestProviderDoesNotReturnNull()
+            => Assert.IsNotNull(Provider.CurrencyDisplayFormats);
+
+        [TestMethod]
+        public void TestProviderReturnsDistinctValues()
+            => Assert.AreEqual(
+                Provider.CurrencyDisplayFormats.Count(),
+                Provider.CurrencyDisplayFormats.Distinct().Count());
+
+        [TestMethod]
+        public void TestProviderReturnsAnItemForEachDisplayFormat()
+            => Assert.AreEqual(
+                Enum.GetValues(typeof(CurrencyDisplayFormat)).Length,
+                Provider.CurrencyDisplayFormats.Count());
+    }
+}
---
+++ BillPath/Providers/CurrencyDisplayFormatProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using BillPath.Models;
+
+namespace BillPath.Providers
+{
+    public class CurrencyDisplayFormatProvider
+    {
+        public IEnumerable<CurrencyDisplayFormat> CurrencyDisplayFormats
+        {
+            get
+            {
+                yield return CurrencyDisplayFormat.Full;
+                yield return CurrencyDisplayFormat.Symbol;
+                yield return CurrencyDisplayFormat.IsoCode;
+            }
+        }
+    }
+}
