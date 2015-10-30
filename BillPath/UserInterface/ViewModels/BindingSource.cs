5f38516ccfe712896737953e9c042453f2202bf3
Andrei Fangli <andrei_fangli@outlook.com>
2015-10-30 20:20:00 +0200

    Task/#49 Added BindingSource.

---
+++ BillPath/UserInterface/ViewModels/BindingSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BillPath.UserInterface.ViewModels
+{
+    public class BindingSource
+        : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (propertyChangedEventArgs == null)
+                throw new ArgumentNullException(nameof(propertyChangedEventArgs));
+
+            PropertyChanged?.Invoke(this, propertyChangedEventArgs);
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+    }
+}
