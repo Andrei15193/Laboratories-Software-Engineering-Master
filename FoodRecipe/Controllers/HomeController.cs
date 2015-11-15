ae874ba7c85743ca9e930c98c16b9d8d291791fe
Andrei Fangli <andrei_fangli@outlook.com>
2015-11-15 19:29:18 +0200

    Created UI prototype.

--- FoodRecipe/Controllers/HomeController.cs
+++ FoodRecipe/Controllers/HomeController.cs
@@ -2,25 +2,15 @@
 
 namespace FoodRecipe.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController
+        : Controller
     {
-        public ActionResult Index()
+        public ActionResult Browse()
         {
             return View();
         }
 
         public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+            => View();
     }
 }
