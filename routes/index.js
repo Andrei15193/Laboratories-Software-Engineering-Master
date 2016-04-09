7af03256061cc8d74d332dba84fc5def0e7358bb
Andrei Fangli <andrei_fangli@outlook.com>
2016-04-09 19:57:59 +0300

    Added dashboard

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ module.exports = require('express')
     .use(require(modules.auth).authenticate)
     .get('/', function(request, response, next) {
         if (response.locals.user)
-            response.render('index/dashboard', { user: request.user });
+            response.render('index/dashboard', { sites: [{ title: 'test site', key: 'site key', description: 'site description' }, { title: 'test site', key: 'site key', description: 'site description' }] });
         else
             response.render('index/index');
     });
